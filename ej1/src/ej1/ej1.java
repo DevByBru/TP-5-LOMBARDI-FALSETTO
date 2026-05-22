aa0fd40433723b69f61cfd007c3804e562264fb0
tahiosdev <falsettobruno@gmail.com>
2026-05-22 14:33:05 -0300

    ej 1

---
+++ ej1/src/ej1/ej1.java
@@ -0,0 +1,15 @@
+public class ej1 {
+    public static void main(String[] args) {
+        int[][] matriz = {
+            {4, 5},
+            {3, 2}
+        };
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print("[" + matriz[i][j] + "]");
+            }
+            System.out.println();
+        }
+    }
+}
